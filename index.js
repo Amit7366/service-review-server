d784f3fac930de1822ec05fbe9064ca1fd234b20
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-11-10 00:04:12 +0600

    install jwt,connect to mongodb

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+
+const app = express();
 const port = process.env.PORT || 5000;
 
 
 app.use(cors());
+app.use(express.json());
 
 
-app.get('/', (req, res) => {
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7dm94fg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+
+
+const run = async ()=>{
+    try{
+      const serviceCollection = client.db('serviceReview').collection('services');
+
+
+
+    }finally{
+      // await client.close();
+    }
+}
+
+run().catch(err => console.log(err));
+
+  app.get('/', (req, res) => {
     res.send('server is running');
   });
+
+
 
 
   app.listen(port, () => {
