9ab02f513225fcc708e3220b94c97abf0a06fe4e
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-11-10 00:46:49 +0600

    get single serice

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 require('dotenv').config();
 
 
@@ -41,6 +41,15 @@ const run = async ()=>{
         const cursor = serviceCollection.find(query);
         const services = await cursor.toArray();
         res.send(services.reverse());
+      })
+
+      app.get('/service/:id',async (req,res) =>{
+          const id = req.params.id;
+          const query = {_id: ObjectId(id)};
+
+          const service = await serviceCollection.findOne(query);
+
+          res.send(service);
       })
 
 
