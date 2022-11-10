eca4858f1ad8c83b51aef93a607b302cd6df9585
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-11-10 12:54:13 +0600

    review by user

--- index.js
+++ index.js
@@ -1,82 +1,88 @@
-const express = require('express');
-const cors = require('cors');
-const jwt = require('jsonwebtoken');
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
-require('dotenv').config();
-
+const express = require("express");
+const cors = require("cors");
+const jwt = require("jsonwebtoken");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.7dm94fg.mongodb.net/?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = async () => {
+  try {
+    const serviceCollection = client.db("serviceReview").collection("services");
+    const reviewCollection = client.db("serviceReview").collection("reviews");
+
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+
+      const services = await cursor.limit(3).toArray();
+
+      res.send(services.reverse());
+    });
+
+    app.get("/allServices", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services.reverse());
+    });
+
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+
+      const service = await serviceCollection.findOne(query);
+
+      res.send(service);
+    });
+
+    app.get("/reviewsById", async (req, res) => {
+      const id = req.query.serviceId;
+      const query = { serviceId: id };
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    app.get("/reviewsByUser", async (req, res) => {
+      const id = req.query.userId;
+      const query = { userId: id };
+      const cursor = reviewCollection.find(query);
+      const reviews = await cursor.toArray();
+      res.send(reviews);
+    });
+
+    app.post("/addReview", async (req, res) => {
+      const review = req.body;
+      const result = await reviewCollection.insertOne(review);
+      res.send(result);
+    });
 
 
 
 
+  } finally {
+    // await client.close();
+  }
+};
 
-const run = async ()=>{
-    try{
-      const serviceCollection = client.db('serviceReview').collection('services');
-      const reviewCollection = client.db('serviceReview').collection('reviews');
+run().catch((err) => console.log(err));
 
+app.get("/", (req, res) => {
+  res.send("server is running");
+});
 
-
-      app.get('/services', async (req,res) =>{
-        const query ={};
-        const cursor = serviceCollection.find(query);
-
-        const services = await cursor.limit(3).toArray();
-
-        res.send(services.reverse());
-      });
-
-      app.get('/allServices', async (req,res) =>{
-        const query ={};
-        const cursor = serviceCollection.find(query);
-        const services = await cursor.toArray();
-        res.send(services.reverse());
-      })
-
-      app.get('/service/:id',async (req,res) =>{
-          const id = req.params.id;
-          const query = {_id: ObjectId(id)};
-
-          const service = await serviceCollection.findOne(query);
-
-          res.send(service);
-      })
-
-      app.get('/reviewsById', async (req,res) =>{
-          const id = req.query.serviceId;
-          const query = {serviceId: id};
-          const cursor =  reviewCollection.find(query);
-          const reviews = await cursor.toArray()
-          res.send(reviews);
-      })
-
-
-
-    }finally{
-      // await client.close();
-    }
-}
-
-run().catch(err => console.log(err));
-
-  app.get('/', (req, res) => {
-    res.send('server is running');
-  });
-
-
-
-
-  app.listen(port, () => {
-    console.log(`listening on port ${port}`);
-  });
+app.listen(port, () => {
+  console.log(`listening on port ${port}`);
+});
