663814bdeb702e897b0195246bcdc2aac5f0ce60
Amit7366 <40818508+Amit7366@users.noreply.github.com>
2022-11-10 14:05:54 +0600

    delete review

--- index.js
+++ index.js
@@ -69,6 +69,13 @@ const run = async () => {
       res.send(result);
     });
 
+    app.delete('/review/:id',async (req,res) =>{
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await reviewCollection.deleteOne(query);
+        res.send(result);
+    });
+
 
 
 
